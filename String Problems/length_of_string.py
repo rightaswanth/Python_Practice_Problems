dd200636ae6f91eecf61cdcd352bb8a9304d0769
Aswanth P V <writetoaswanth@gmail.com>
2024-05-27 09:39:00 +0530

    symmetric string problem

--- String Problems/length_of_string.py
+++ String Problems/length_of_string.py
@@ -3,21 +3,35 @@
 
 # Method - 1
 
-fruit = "apple"
-length = len(fruit)
+# fruit = "apple"
+# length = len(fruit)
 
-print("The length of the string is :",length)
+# print("The length of the string is :",length)
 
 # Method - 2
 
-fruit = "apple"
-length = 0
+# fruit = "apple"
+# length = 0
 
-for f in fruit:
-    length += 1
+# for f in fruit:
+#     length += 1
 
 # Method - 3
     
+fruit = "apple"
+length = 0
+i = 0
 
+while i <= length:
+    try:
+        fruit[i]
+        i += 1
+        length = i
+    except IndexError:
+        length = length + 1
+        print("The length of the string is ",length + 1)
+
+    
+ 
 
 
